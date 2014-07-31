86e15c26785129f834eda2302bad4a6082647f9d
Nathan Rajlich <nathan@tootallnate.net>
2014-07-30 18:21:14 -0700

    test: add a few more test cases

--- test/test.js
+++ test/test.js
@@ -25,6 +25,8 @@ describe('save-range', function () {
 
     // test that the DOM is back to original state
     assert.equal(html, div.innerHTML);
+
+    // TODO: test that the returned Range is properly set up
   });
 
   it('save() and load() a `collapsed` Range instance', function () {
@@ -58,6 +60,57 @@ describe('save-range', function () {
     assert.equal('B', r2.endContainer.nodeName);
     assert.equal(1, r2.endOffset);
     assert(r2.collapsed);
+  });
+
+  it('save() and load() a Range through HTML serialization', function () {
+    var div = document.createElement('div');
+    div.innerHTML = '<b>f<i>o<u>o bar</u></i></b> baz';
+    var html = div.innerHTML;
+    var b = div.firstChild;
+
+    var range = document.createRange();
+    range.setStart(div.firstChild.childNodes[1].childNodes[1].firstChild, 1);
+    range.setEnd(div.childNodes[1], 3);
+
+    // save Range
+    var info = saveRange(range);
+
+    // now destory the HTML DOM references on the `info` object by re-parsing the
+    // `innerHTML` on the div
+    div.innerHTML = html;
+
+    // test that the previous <b> is now detached
+    assert(!b.parentNode);
+    assert(b !== div.firstChild);
+
+    // load Range
+    var r2 = saveRange.load(info);
+
+    // TODO: test that the returned Range is properly set up
+  });
+
+  it('save() and load() a Range with explicit `parent` given', function () {
+    var div = document.createElement('div');
+    div.innerHTML = '<b>f<i>o<u>o bar</u></i></b> baz';
+
+    var range = document.createRange();
+    range.setStart(div.firstChild.childNodes[1].childNodes[1].firstChild, 1);
+    range.setEnd(div.childNodes[1], 3);
+
+    // save Range
+    var info = saveRange(range);
+
+    // now transfer the child nodes of the <div> into a new DOM element
+    var em = document.createElement('em');
+    while (div.firstChild) {
+      em.appendChild(div.firstChild);
+    }
+
+    // load Range
+    var r2 = saveRange.load(info, em);
+
+    // test that the <em> is now the "common ancestor container"
+    assert(r2.commonAncestorContainer === em);
   });
 
 });
